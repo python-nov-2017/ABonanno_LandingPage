9d361ac432e863538b26f4e2bc6c66e6b5231fb9
Anthony <abonanno2010@gmail.com>
2017-11-10 12:54:17 -0500

    end of assignment commit

--- server.py
+++ server.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template("ninjas.html")
+
+@app.route('/dojo')
+def dojoform():
+    return render_template("dojo.html")
+
+@app.route('/dojo/new', methods=["POST"])
+def dojo():
+    print request.form["name"]
+    print request.form["hobby"]
+    return "Thanks for the data!"
+
+
 
 app.run(debug=True)
